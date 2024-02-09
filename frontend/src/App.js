506d94a2363ed94a44057ee3889766c57d45a959
Nipuni-De-Silva <nipunisha274@gmail.com>
2024-02-10 01:05:35 +0530

    Initialize the frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,44 @@
-import React from 'react'
+// import {Container} from 'react-bootstrap'
+// import Header from './components/Header'
+// import Footer from './components/Footer'
+// import HomeScreen from './screens/HomeScreen'
+
+// import React from 'react'
+
+// const App = () => {
+//   return (
+//     <>
+//       <Header />
+//       <main className='py-3'>
+//         <Container>
+//           <HomeScreen />
+//         </Container>
+//       </main>
+//       <Footer />
+//     </>
+//   )
+// }
+
+// export default App
+
+import { Container } from 'react-bootstrap';
+import { Outlet } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import React from 'react';
 
 const App = () => {
   return (
-    <div>
-      <h1>Pro Shop</h1>
-      <p>Welcome to Proshop</p>
-    </div>
-  )
-}
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Outlet />  {/* Child routes are rendered here . When we navigate to /product/:id, the ProductScreen component is rendered in the place of outlet. if no nested route is matches. nothing is rendering in place of the outlet */}
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
+};
 
-export default App
+export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,60 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import {
+//   createBrowserRouter,
+//   createRoutesFromElements,
+//   Route,
+//   RouterProvider
+// } from 'react-router-dom'
+// // import 'bootstrap/dist/css/bootstrap.min.css';
+// import './assets/styles/index.css';
+// import './assets/styles/bootstrap.custom.css'
+// import App from './App';
+// import HomeScreen from './screens/HomeScreen';
+// import ProductScreen from './screens/ProductScreen';
+// import reportWebVitals from './reportWebVitals';
+
+// const router = createBrowserRouter(
+//   createRoutesFromElements(
+//     <Route path='/' element={<App />}>
+//       <Route index={true} path="/" element={<HomeScreen />} />
+//       <Route path='/product/:id' element={<ProductScreen />} />
+//     </Route>
+//   )
+// )
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <RouterProvider router={router} />
+//   </React.StrictMode>
+// );
+
+// reportWebVitals();
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './assets/styles/index.css';
+import './assets/styles/bootstrap.custom.css'
 import App from './App';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<HomeScreen />} />
+          <Route path="product/:id" element={<ProductScreen />} />
+        </Route>
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
