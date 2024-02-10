62a997d2082474e3c2ea38e9a9463cb6030a6c45
Nipuni-De-Silva <nipunisha274@gmail.com>
2024-02-10 10:04:16 +0530

    testing API end points

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const products = require('./data/products');
 const app = express();
 
 const port = 5000;
@@ -7,6 +8,14 @@ app.get('/', (req, res) => {
     res.send('Hello World');
 })
 
+app.get('/api/products', (req, res) => {
+    res.json(products)
+})
+
+app.get('/api/products/:id', (req, res) => {
+    const product = products.find((p) => p._id === req.params.id);
+    res.json(product);
+})
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
